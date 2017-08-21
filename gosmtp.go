f576de7b3d501d1ef8acd5e5599147c4725caca2
Dmitry Orlov <feanarokurufinve@mail.ru>
2017-08-21 16:11:49 +0300

    Initial CLI

---
+++ gosmtp.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	log.Fatal(listenTCP())
+}
+
+func listenTCP() error {
+	var listenAddr string
+	flag.StringVar(&listenAddr, "listen-addr", "0.0.0.0:25", "address to listen")
+	flag.Parse()
+
+	l, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return fmt.Errorf("error listening on %q: %s", listenAddr, err)
+	}
+	defer func() {
+		closeErr := l.Close()
+		if closeErr != nil {
+			log.Printf("can't close listen socket: %s", err)
+		}
+	}()
+
+	fmt.Printf("Listening on %q\n", listenAddr)
+
+	for {
+		conn, err := l.Accept()
+		defer conn.Close()
+		log.Printf("Connection from %s...", conn.RemoteAddr())
+
+		if err != nil {
+			fmt.Printf("Error accepting connection %q: %s", listenAddr, err)
+			time.Sleep(100 * time.Millisecond)
+			continue
+		} else {
+			log.Printf("...successful (%s)", conn.RemoteAddr())
+		}
+
+		if err := handleRequest(conn); err != nil {
+			log.Printf("Stopped handling requests from %s: %s", conn.RemoteAddr(), err)
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+	}
+}
+
+func handleRequest(conn net.Conn) error {
+	buf := make([]byte, 1024)
+	reqLen, err := conn.Read(buf)
+	for ; err == nil; reqLen, err = conn.Read(buf) {
+		log.Printf("got request of len %d bytes from %s: %q", reqLen, conn.RemoteAddr(), buf[:reqLen])
+		if _, err := conn.Write(dispatchResponse(buf[:reqLen])); err != nil {
+			return fmt.Errorf("can't write to connection: %s", err)
+		}
+	}
+	if err != nil {
+		return fmt.Errorf("can't read data from connection: %s", err)
+	}
+	return nil
+}
+
+func dispatchResponse(command []byte) []byte {
+	return []byte("Message received.\n")
+}
