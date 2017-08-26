654e1fb9817c8a650f035c06ad89b30384c01945
Dmitry Orlov <feanarokurufinve@mail.ru>
2017-08-26 10:20:39 +0300

    Package name typo - confreaders now

--- confreaders/json.go
+++ confreaders/json.go
@@ -1,4 +1,4 @@
-package conf-readers
+package confreaders
 
 import (
 	"fmt"
@@ -7,22 +7,24 @@ import (
 	"log"
 )
 
-func ReadYAMLConfig(filename string) map[string]string {
+func ReadJSONConfig(filename string) map[string]string {
 	content, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-    content_array := strings.Split(string(content), "\n")
+    content_array := strings.Split(string(content), ",")
     content_map := make(map[string]string)
     for i := 0; i < len(content_array); i++ {
         row := strings.Split(content_array[i], ": ")
         if len(row) == 2 {
-            content_map[row[0]] = row[1]
+			key := strings.Trim(row[0], "\" \n\r,{}")
+			val := strings.Trim(row[1], "\" \n\r,{}")
+            content_map[key] = val
         }
     }
 	return content_map
 }
 
 func main() {
-    fmt.Println(readConfig("sample.yaml"))
+    fmt.Println(readConfig("sample.json"))
 }
--- confreaders/yaml.go
+++ confreaders/yaml.go
@@ -1,4 +1,4 @@
-package conf-readers
+package confreaders
 
 import (
 	"fmt"
@@ -7,24 +7,22 @@ import (
 	"log"
 )
 
-func ReadJSONConfig(filename string) map[string]string {
+func ReadYAMLConfig(filename string) map[string]string {
 	content, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-    content_array := strings.Split(string(content), ",")
+    content_array := strings.Split(string(content), "\n")
     content_map := make(map[string]string)
     for i := 0; i < len(content_array); i++ {
         row := strings.Split(content_array[i], ": ")
         if len(row) == 2 {
-			key := strings.Trim(row[0], "\" \n\r,{}")
-			val := strings.Trim(row[1], "\" \n\r,{}")
-            content_map[key] = val
+            content_map[row[0]] = row[1]
         }
     }
 	return content_map
 }
 
 func main() {
-    fmt.Println(readConfig("sample.json"))
+    fmt.Println(readConfig("sample.yaml"))
 }
